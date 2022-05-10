863e3d3ce95f64519fec9d897c9b51414e703450
Rajesh Majumdar <rajeshmjmdr02@gmail.com>
2022-05-10 23:17:44 +0530

    Adding fibonacci sequence

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input.");
+
+    let number: u32 = input
+        .trim()
+        .parse()
+        .expect("Input is not a number.");
+
+    fibonacci(number);
 }
+
+fn fibonacci(mut x: u32) {
+    let mut first: u32 = 0;
+    let mut second: u32 = 1;
+    println!("1");
+    while x > 1 {
+        let result = {
+            first + second
+        };
+        first = second;
+        second = result;
+        println!("{}", result);
+        x -= 1;
+    }
+}
+        
