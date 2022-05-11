aa1615d73f83b93757932384abcdd207c3576791
Rajesh Majumdar <rajeshmjmdr02@gmail.com>
2022-05-11 22:11:45 +0530

    Mutable references

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
 
     references();
-
+    mut_refs();
 }
 
 fn references() {
@@ -15,4 +15,29 @@ fn calculate_len(s: &String) -> usize {
     s.len()
         // We aren't using ; at the end, because we want this value to be returned
 }
-    
+
+//fn mut_refs() {
+//    let s = String::from("Hello");
+//    change(&s);
+
+//}
+
+//fn change(s: &String) {
+//    s.push_str(", world!");
+//}
+
+// if we run the above func, it should throw us an error because we are getting an
+// reference and then we are modifying, and just like variables are immutable by
+// default in rust, references are also immutable. So for it to work we need to make
+// it mutable.
+
+fn mut_refs() {
+    let mut s = String::from("Hello");
+    change(&mut s);
+}
+
+fn change(s: &mut String) {
+    s.push_str(", world!");
+    println!("{}", s);
+}
+
