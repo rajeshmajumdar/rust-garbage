0e3f31398743566062f4db596e6ce447bcb50a67
Rajesh Majumdar <rajeshmjmdr02@gmail.com>
2022-05-11 22:34:02 +0530

    Mutability and immutability

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
 
     references();
     mut_refs();
+    multiple_mut();
 }
 
 fn references() {
@@ -41,3 +42,24 @@ fn change(s: &mut String) {
     println!("{}", s);
 }
 
+fn multiple_mut() {
+    let mut s = String::from("Hemlo");
+    //let r1 = &mut s;
+    //let r1 = &s1;
+    //let r2 = &mut s;
+    // this code should throw us an error since we are creating multiple mutable refs of the same
+    // piece of data. In rust, we are only allowed to make one mutable reference to particular
+    // piece of data at a time.
+    // Second piece of code would also fail compiling since, here we are first creating a immutable
+    // refs and later we are creating a mutable refs, and rust doesn't allow that
+
+    //println!("{} {}", r1, r2);
+    
+    let r1 = &s;
+    let r2 = &s;
+    println!("We are using the immutable refs here.: {} {}", r1, r2);
+
+    let r3 = &mut s;
+    println!("Now we should be able to create a mutable refs.: {}", r3);
+
+}
