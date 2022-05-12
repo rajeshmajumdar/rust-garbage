6ee00d987bb6f0b5d7b571936da21760ab803a6b
Rajesh Majumdar <rajeshmjmdr02@gmail.com>
2022-05-12 11:54:24 +0530

    Using structs

---
+++ using_structs/src/main.rs
@@ -0,0 +1,26 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 10,
+        height: 10,
+    };
+
+    //area(&rect);
+    //println!("The area of the rect. is {}", area(&rect));
+    println!("Rect: {:?}", rect);
+}
+
+fn area(rect: &Rectangle) -> u32 {
+    //println!("The area of the rect. is {}", rect.width * rect.height);
+    rect.width * rect.height
+}
+
+// We are using {:?} to print out debug information in the println! macro, we can
+// also use dbg! macro instead to acheive the same result, the only difference is
+// in println! we are outputting it through stdout, while dbg! macro output it 
+// through the stderr, which might come in handy somewhere.
